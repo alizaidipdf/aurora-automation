c2e9ed495f0cf1aa509da889fb4c510c7cf2fcfd
Ali Zaidi (PDF) <aliz@pdf.com>
2024-11-20 14:15:43 -0800

    Initial commit with project setup

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using Xunit;
+
+namespace aurora_automation
+{
+    public class Class1
+    {
+        [Fact]
+        public void addTwoPlusTwo()
+        {
+            int a = 2;
+            int b = 2;
+            int c = a + b;
+            Assert.Equal(c, 4);
+
+        }
+        [Fact]
+        public void addThreePlusFive()
+        {
+            int a = 3;
+            int b = 5;
+            int c = a + b;
+            Assert.Equal(c, 7);
+
+        }
+
+    }
+}
